35a45c8d37f2f1a9d043b99cb3d109a137b58d83
Kim Andersson <Kimpabooy@live.com>
2024-10-18 09:49:38 +0200

    Circle class done

---
+++ IntroTillOOP/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IntroTillOOP
+{
+    internal class Circle
+    {
+        private double _pi = Math.PI;
+        private int _radius;
+
+        public Circle(int radius)
+        {
+            _radius = radius;
+        }
+        public double GetArea() 
+        {
+            return _pi;
+        }
+        
+    }
+}
